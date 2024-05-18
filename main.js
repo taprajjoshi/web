16d855d37740c754be7eaaf5e1cfd69e87b58917
taprajjoshi <joshitapraj64@gmail.com>
2024-05-18 14:40:28 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function updateClock() {
+    var nepalTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Kathmandu"});
+    var options = {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true};
+    var formattedTime = new Date(nepalTime).toLocaleTimeString('en-US', options);
+    var date = new Date(nepalTime).toLocaleDateString('en-US');
+    document.getElementById('clock').innerHTML = formattedTime + '<br>' + date;
+}
+
+updateClock(); // Initial call
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+
+document.getElementById("notesButton").addEventListener("click", function() {
+    window.location.href = "https://joshibhuwan1.github.io/notes";
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('notesButton').addEventListener('click', function() {
+        alert('Notes button clicked!');
+    });
+});
